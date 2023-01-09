785bcd0a136fb8223e8585bb0d199fe3363459b9
Kazuki Hara <kazuki.hara.dw3@gmail.com>
2023-01-09 18:22:25 +0900

    Add Test

--- apiv1/tests.py
+++ apiv1/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .serializers import ResultSerializer
 
-# Create your tests here.
+class TestResultSerializer(TestCase):
+  """ResultSerializerのテスト"""
+
+  def test_input_valid(self):
+    """入力データのバリデーション(OK)"""
+
+    input_data = {
+      'scan_time': '2023-01-09 11:02:07',
+      'hostname': 'ASDFGHJK',
+      'emocheck_version': '0.1.0',
+      'is_infected': 'yes'
+    }
+
+    serializer = ResultSerializer(data=input_data)
+
+    self.assertEqual(serializer.is_valid(), True)
